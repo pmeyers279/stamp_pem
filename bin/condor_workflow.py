57bbde40f6fe9739673ef98dc5bd8c22f02e4fc2
Patrick Meyers <pmeyers279@gmail.com>
2015-09-25 15:53:06 -0500

    fixed typo

--- bin/condor_workflow.py
+++ bin/condor_workflow.py
@@ -46,7 +46,7 @@ def build_arg(env_params, run_params, st, et):
 params = parse_command_line()
 
 pipeline_dict = coh_io.read_pipeline_ini(params.ini)
-env_params, run_params = coh_io.check_ini_params(pipeline_dict['env'])
+env_params, run_params = coh_io.check_ini_params(pipeline_dict)
 channel_dict = coh_io.read_list(env_params['list'])
 
 try:
@@ -71,75 +71,73 @@ segs = DataQualityFlag.query_dqsegdb(flag, st, et)
 seg_file = coh_io.create_coherence_data_filename(flag, 'SEGMENTS', st, et)
 segs.write('%s.xml.gz' % (seg_file))
 
-for seg in segs.active:
 
-    datajob = pipeline.CondorDAGJob('vanilla', env_params['executable'])
-    datajob2 = pipeline.CondorDAGJob(
-        'vanilla', env_params['combine_executable'])
-    dag = pipeline.CondorDAGJob(
-        '/usr1/%s/$(subsystem).log' % (env_params['user']))
-    coh_io.create_directory_structure(
-        channel_dict.keys(), st, directory=env_params['base_directory'])
+datajob = pipeline.CondorDAGJob('vanilla', env_params['executable'])
+datajob2 = pipeline.CondorDAGJob(
+    'vanilla', env_params['combine_executable'])
+dag = pipeline.CondorDAGJob(
+    '/usr1/%s/$(subsystem).log' % (env_params['user']))
+coh_io.create_directory_structure(
+    channel_dict.keys(), st, directory=env_params['base_directory'])
 
-    dag_dir = coh_io.get_directory_structure(
-        'DAGS', st, env_params['base_directory'])
+dag_dir = coh_io.get_directory_structure(
+    'DAGS', st, env_params['base_directory'])
 
-    for subsystem in channel_dict.keys():
-        for seg in segs.active:
-            seg_st = seg[0].seconds
-            seg_et = seg[1].seconds
-            job = pipeline.CondorDAGJob('vanilla', env_params['executable'])
-            job.set_sub_file('%s/%s-%d-%d.sub',
-                             dag_dir, darm_channel.replace(':', '-'),
-                             seg_st, seg_et)
-            job.set_stderr_file(
-                '/usr1/%s/$(subsystem).err' % (env_params['user']))
-            job.set_stdout_file(
-                '/usr1/%s/$(subsystem).out' % (env_params['user']))
-            node = pipeline.CondorDAGNode(job)
-            node.add_macro("subsystem", subsystem)
-            node.add_macro("st", seg_st)
-            node.add_macro("et", seg_et)
-            dag.add_node(node)
-    for subsystem in channel_dict.keys():
-        job = pipeline.CondorDAGJob(
-            'vanilla', env_params['combine_executable'])
-        job.set_sub_file('%s/combine_jobs-%d-%d.sub', dag_dir, st, et)
+for subsystem in channel_dict.keys():
+    for seg in segs.active:
+        seg_st = seg[0].seconds
+        seg_et = seg[1].seconds
+        job = pipeline.CondorDAGJob('vanilla', env_params['executable'])
+        job.set_sub_file('%s/%s-%d-%d.sub',
+                         dag_dir, darm_channel.replace(':', '-'),
+                         seg_st, seg_et)
         job.set_stderr_file(
-            '/usr1/%s/$(subsystem)-combine.err' % env_params['user'])
+            '/usr1/%s/$(subsystem).err' % (env_params['user']))
         job.set_stdout_file(
-            '/usr1/%s/$(subsystem)-combine.out' % env_params['user'])
+            '/usr1/%s/$(subsystem).out' % (env_params['user']))
         node = pipeline.CondorDAGNode(job)
-        node.add_macro('subsystem', subsystem)
+        node.add_macro("subsystem", subsystem)
+        node.add_macro("st", seg_st)
+        node.add_macro("et", seg_et)
         dag.add_node(node)
-
-    dagName = '%s/%s-%d-%d' % (
-        dag_dir, darm_channel.replace(':', '-'), st, et)
-
-    # datajob info
-    datajob.set_sub_file('%s/%s-%d-%d.sub',
-                         darm_channel.replace(':', '-'), st, et)
-    datajob.set_stderr_file(
-        '/usr1/%s/$(subsystem).err' % (env_params['user']))
-    datajob.set_stdout_file(
-        '/usr1/%s/$(subsystem).out' % (env_params['user']))
-    datajob.set_log_file(
-        '/usr1/%s/$(subsystem).out' % (env_params['user']))
-
-    # combine jobs post processing info
-    datajob2.set_sub_file('%s/combine_jobs-%d-%d.sub', dag_dir, st, et)
-    datajob2.set_stderr_file(
+for subsystem in channel_dict.keys():
+    job = pipeline.CondorDAGJob(
+        'vanilla', env_params['combine_executable'])
+    job.set_sub_file('%s/combine_jobs-%d-%d.sub', dag_dir, st, et)
+    job.set_stderr_file(
         '/usr1/%s/$(subsystem)-combine.err' % env_params['user'])
-    datajob2.set_stdout_file(
+    job.set_stdout_file(
         '/usr1/%s/$(subsystem)-combine.out' % env_params['user'])
-    datajob2.set_log_file(
-        '/usr1/%s/$(subsystem)-combine.log' % env_params['user'])
-    arg = build_arg(env_params, run_params)
-    print 'ARG = %s' % arg
-    datajob.add_arg(arg)
-    datajob2.add_arg('-s %s -e %s --subsystem $(subsystem) --darm-channel %s')
-    datajob.write_sub_file()
-    datajob2.write_sub_file()
-    dag.set_dag_file(dagName)
-    dag.write_dag
+    node = pipeline.CondorDAGNode(job)
+    node.add_macro('subsystem', subsystem)
+    dag.add_node(node)
 
+dagName = '%s/%s-%d-%d' % (
+    dag_dir, darm_channel.replace(':', '-'), st, et)
+
+# datajob info
+datajob.set_sub_file('%s/%s-%d-%d.sub',
+                     darm_channel.replace(':', '-'), st, et)
+datajob.set_stderr_file(
+    '/usr1/%s/$(subsystem).err' % (env_params['user']))
+datajob.set_stdout_file(
+    '/usr1/%s/$(subsystem).out' % (env_params['user']))
+datajob.set_log_file(
+    '/usr1/%s/$(subsystem).out' % (env_params['user']))
+
+# combine jobs post processing info
+datajob2.set_sub_file('%s/combine_jobs-%d-%d.sub', dag_dir, st, et)
+datajob2.set_stderr_file(
+    '/usr1/%s/$(subsystem)-combine.err' % env_params['user'])
+datajob2.set_stdout_file(
+    '/usr1/%s/$(subsystem)-combine.out' % env_params['user'])
+datajob2.set_log_file(
+    '/usr1/%s/$(subsystem)-combine.log' % env_params['user'])
+arg = build_arg(env_params, run_params)
+print 'ARG = %s' % arg
+datajob.add_arg(arg)
+datajob2.add_arg('-s %s -e %s --subsystem $(subsystem) --darm-channel %s')
+datajob.write_sub_file()
+datajob2.write_sub_file()
+dag.set_dag_file(dagName)
+dag.write_dag
